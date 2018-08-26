1c040535b2b1f365d778b1695b7984ffdbdbae5b
irissun9602 <iris9602@naver.com>
2018-08-27 02:59:49 +0900

    refactor: jpa에서 mybatis로 변경, mapper.xml 추가

--- data_L/src/main/java/data/laundry/dto/All_tab_cols.java
+++ data_L/src/main/java/data/laundry/dto/All_tab_cols.java
@@ -1,24 +1,13 @@
 package data.laundry.dto;
 //http://blog.naver.com/PostView.nhn?blogId=scw0531&logNo=221105180348&parentCategoryNo=&categoryNo=28&viewDate=&isShowPopularPosts=false&from=postView
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity
-@Table(name="all_tab_cols")
 public class All_tab_cols {
 
-	@Id
-	@Column(name="column_id")
 	private int column_id;
-	
-	@Column(name="column_name")
 	private String column_name;
-	
-	@Column(name="data_type")
 	private String data_type;
 
+	
 	public int getColumn_id() {
 		return column_id;
 	}
@@ -42,9 +31,5 @@ public class All_tab_cols {
 	public void setData_type(String data_type) {
 		this.data_type = data_type;
 	}
-	
-	
-	
-	
-	
+
 }
