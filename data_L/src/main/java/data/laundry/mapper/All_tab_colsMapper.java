21ebeb90b79acbf87355af6674d737183ea20712
irissun9602 <iris9602@naver.com>
2018-08-31 01:44:59 +0900

    feat: 컬럼 이름으로 검색 기능

--- data_L/src/main/java/data/laundry/mapper/All_tab_colsMapper.java
+++ data_L/src/main/java/data/laundry/mapper/All_tab_colsMapper.java
@@ -12,7 +12,7 @@ public interface All_tab_colsMapper{
 	List<All_tab_cols> findAll();
 	
 	List<All_tab_cols> findByColumn_id(int column_id);
-	
+	List<All_tab_cols> findByColumn_name(String column_name);
 	List<All_tab_cols> findAllNumber();
 	
 	
