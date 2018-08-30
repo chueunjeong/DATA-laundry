06a21bbc9dad21132f737408edd2cd09c7b40f43
irissun9602 <iris9602@naver.com>
2018-08-31 01:45:31 +0900

    feat: Sample dto, Mapper 작성 후 출력 확인

---
+++ data_L/src/main/java/data/laundry/mapper/SampleMapper.java
@@ -0,0 +1,13 @@
+package data.laundry.mapper;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import data.laundry.dto.Sample;
+
+@Mapper
+public interface SampleMapper {
+	
+	List<Sample> findAll();
+
+}
