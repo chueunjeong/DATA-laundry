a9d976034b7eb72cea6e157eae188a73913d2b74
irissun9602 <iris9602@naver.com>
2018-09-19 04:13:07 +0900

    feat: 레코드 조회 메소드 추가

--- data_L/src/main/java/data/laundry/controller/ApiController.java
+++ data_L/src/main/java/data/laundry/controller/ApiController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import data.laundry.dto.All_tab_cols;
-import data.laundry.dto.Code;
+import data.laundry.dto.Standarzation;
 import data.laundry.dto.Sample;
-import data.laundry.mapper.All_tab_colsMapper;
-import data.laundry.mapper.CodeMapper;
+
+import data.laundry.dto.Meta_column;
+import data.laundry.mapper.StandarzationMapper;
+import data.laundry.mapper.Meta_columnMapper;
 import data.laundry.mapper.SampleMapper;
 
 @RestController
@@ -21,38 +22,40 @@ import data.laundry.mapper.SampleMapper;
 public class ApiController {
 
 	@Autowired
-	All_tab_colsMapper columnMapper;
+	Meta_columnMapper columnMapper;
 	@Autowired
 	SampleMapper sampleMapper;
 	@Autowired
-	CodeMapper codeMapper;
+	StandarzationMapper standarzationMapper;
+	
+
 	@RequestMapping("columns")
-	public List<All_tab_cols> columns() {
+	public List<Meta_column> columns() {
 		return columnMapper.findAll();
 	}
 	
 	@RequestMapping("tables")
-	public List<All_tab_cols> tables() {
+	public List<Meta_column> tables() {
 		return columnMapper.groupByTable();
 	}
 	
 
 	//id로 조회
 	@RequestMapping("columns/id/{id}")
-	public List<All_tab_cols> column_id(@PathVariable("id") int id) {
-		return columnMapper.findByColumn_id(id);
+	public List<Meta_column> column_id(@PathVariable("id") int id) {
+		return columnMapper.findById(id);
 	}
 	
 	//name으로 조회 대소문자 구별함.
 	@RequestMapping("columns/column_name/{name}")
-	public List<All_tab_cols> column_name(@PathVariable("name") String name) {
+	public List<Meta_column> column_name(@PathVariable("name") String name) {
 		return columnMapper.findByColumn_name(name);
 	}
 
 	//name으로 조회 대소문자 구별함.
 	@RequestMapping("columns/table_name/{name}")
-	public List<All_tab_cols> table_name(@PathVariable("name") String name) {
-		return columnMapper.findByTable_name(name);
+	public List<Standarzation> table_name(@PathVariable("name") String name) {
+		return standarzationMapper.showDomain(name);
 	}
 
 	//name으로 조회 대소문자 구별함.
@@ -66,29 +69,35 @@ public class ApiController {
 		
 	
 	
-	//type이 "number"인 것만 조회
-	@RequestMapping("columns/number")
-	public List<All_tab_cols> columns_num() {
-		return columnMapper.findAllNumber();
-	}
 
-	@RequestMapping("samples")
-	public List<Sample> samples() {
+	@RequestMapping("record/SAMPLE")
+	public List<Sample> record_samples() {
 		return sampleMapper.findAll();
 	}
 	
 	
 	@RequestMapping("codes")
-	public List<Code> codes() {
-		return codeMapper.findAll();
+	public List<Standarzation> codes() {
+		return standarzationMapper.findAll();
 	}
 	
 	
 	
 	@RequestMapping("code/{group_code_id}")
-	public List<Code> findByGroup(@PathVariable("group_code_id") int group_code_id) {
-		return codeMapper.findByGroup_code_id(group_code_id);
+	public List<Standarzation> findByGroup(@PathVariable("group_code_id") int group_code_id) {
+		return standarzationMapper.findByGroup_code_id(group_code_id);
 	}
+	
+	/*@RequestMapping("sample/{column_name}")
+	public List<Sample> findByGroup(@PathVariable("column_name") String column_name) {
+		
+		
+		
+		return columnMapper.findByYn();
+	}*/
+	
+	
+	
 	
 	
 }
