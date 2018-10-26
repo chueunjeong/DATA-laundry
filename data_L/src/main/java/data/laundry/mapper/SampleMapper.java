b282d5ba3245a963c63799730ed2f34762986f62
irissun9602 <iris9602@naver.com>
2018-10-26 12:35:42 +0900

    feat: 필터링 조건 yes no

--- data_L/src/main/java/data/laundry/mapper/SampleMapper.java
+++ data_L/src/main/java/data/laundry/mapper/SampleMapper.java
@@ -6,13 +6,13 @@ import org.apache.ibatis.annotations.Mapper;
 
 import data.laundry.dto.ParameterType;
 import data.laundry.dto.Sample;
-import data.laundry.dto.Standarzation;
+import data.laundry.dto.Standardization;
 
 @Mapper
 public interface SampleMapper {
 	
 	List<Sample> findAll();
-	List<HashMap<String, Object>> findByYN(Standarzation standarzaion);
-	List<HashMap<String, Object>> findByYesNo(Standarzation standarzaion);
+	List<HashMap<String, Object>> findByYN(Standardization standarzaion);
+	List<HashMap<String, Object>> findByYesNo(Standardization standarzaion);
 
 }
