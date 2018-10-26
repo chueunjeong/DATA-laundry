c611fa91cc47ce7c41e85d544be70402a4194d05
irissun9602 <iris9602@naver.com>
2018-10-26 15:16:19 +0900

    fix: 필요없는 import 삭제

--- data_L/src/main/java/data/laundry/mapper/SampleMapper.java
+++ data_L/src/main/java/data/laundry/mapper/SampleMapper.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import data.laundry.dto.ParameterType;
 import data.laundry.dto.Sample;
 import data.laundry.dto.Standardization;
 
